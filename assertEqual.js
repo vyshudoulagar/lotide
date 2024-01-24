b6dd87166260146b659bfa8b9a73908da0630466
WishNavy <du.vyshu@gmail.com>
2024-01-24 14:40:57 -0800

    created an assertion function to check if the values are equal

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🟢🟢🟢 Assertion Passed: " + actual + ' === ' + expected);
+  } else {
+    console.log('🔴🔴🔴 Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+};
+
+assertEqual('Bootcamp', 'Bootcamp');
+assertEqual(1, 3);

382cfb9cc77121ad38e3214180a67ec0483cf50b
WishNavy <du.vyshu@gmail.com>
2024-01-24 19:19:37 -0800

    created a head function

---
+++ head.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(arr) {
+  return arr[0];
+};
+  
+assertEqual('Bootcamp', 'Bootcamp');
+assertEqual(1, 3);
+assertEqual('Day', 'Night');
+assertEqual(5, 5);
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([6]), 6);
+assertEqual(head([]), 3);
+

dd7057d1175e6337cea5eff952df94c154ebe55c
WishNavy <du.vyshu@gmail.com>
2024-01-28 11:41:55 -0800

    made some changes

--- eqArrays.js
+++ eqArrays.js
@@ -11,9 +11,9 @@ const assertEqual = function(actual, expected) {
       for(let i = 0; i < arr1.length; i++){
         if(arr1[i] !== arr2[i]){
             return false;
-        }
-        return true;    
+        }    
       }
+      return true;
     }else{
         return false;
     }

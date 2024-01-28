b6424baddc47c9f48b6a46c9ec46ab20264b853d
WishNavy <du.vyshu@gmail.com>
2024-01-28 10:57:19 -0800

    corrected the funtion to return only one boolean value

--- eqArrays.js
+++ eqArrays.js
@@ -7,19 +7,26 @@ const assertEqual = function(actual, expected) {
   };
 
   const eqArrays = function(arr1, arr2){
-    for(let i = 0; i < arr1.length; i++){
-        if(arr1[i] === arr2[i]){
-            return true;
+    if(arr1.length === arr2.length){
+      for(let i = 0; i < arr1.length; i++){
+        var result = true;
+        if(arr1[i] === arr2[i] && result === true){
+            result = true 
         }else{
-            return false;
+            result = false;
         }
+        
+      }
+      return result;
+    }else{
+        return false;
     }
   }
 
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
+console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS

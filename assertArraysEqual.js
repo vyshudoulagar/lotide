7a950f3579be56ee021eb9240747430efbfeed70
WishNavy <du.vyshu@gmail.com>
2024-01-28 19:40:27 -0800

    removed === true in if stmt

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -12,7 +12,7 @@ const eqArrays = function(arr1, arr2){
   }
 
   const assertArraysEqual = function(array1,array2){
-    if(eqArrays(array1,array2) === true){
+    if(eqArrays(array1,array2)){
         console.log(`🟢🟢🟢 Assertion Passed: ${array1} === ${array2}`);
     }else{
         console.log(`🔴🔴🔴 Assertion Failed: ${array1} !== ${array2}`);

514f6154b20113a4029479a6d933d0163db4ff3e
WishNavy <du.vyshu@gmail.com>
2024-01-28 19:19:23 -0800

    created without func

---
+++ without.js
@@ -0,0 +1,35 @@
+const assertArraysEqual = function(array1,array2){
+    if(eqArrays(array1,array2)){
+        console.log(`🟢🟢🟢 Assertion Passed: ${array1} === ${array2}`);
+    }else{
+        console.log(`🔴🔴🔴 Assertion Failed: ${array1} !== ${array2}`);
+    }
+  }
+
+  const eqArrays = function(arr1, arr2){
+    if(arr1.length === arr2.length){
+      for(let i = 0; i < arr1.length; i++){
+        if(arr1[i] !== arr2[i]){
+            return false;
+        }    
+      }
+      return true;
+    }else{
+        return false;
+    }
+  }
+
+  const without = function(source, itemsToRemove){
+    const result = source.filter(item => !itemsToRemove.includes(item));
+    console.log(result);
+  }
+
+  without([1, 2, 3], [1]) // => [2, 3]
+  without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
+  without([1, 2, 3, 4], [1, 2, 3, 4])
+
+  const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]);
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
+ 

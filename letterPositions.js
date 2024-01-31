a376984322074fc1658586bf204d46ae31dda894
WishNavy <du.vyshu@gmail.com>
2024-01-30 16:33:33 -0800

    added a variable letter

--- letterPositions.js
+++ letterPositions.js
@@ -22,11 +22,12 @@ const eqArrays = function(arr1, arr2) {
 const letterPositions = function(sentence) {
   const results = {};
   for (let i = 0; i < sentence.length; i++) {
-    if (sentence[i] !== " ") {
-      if (results[sentence[i]]) {
-        results[sentence[i]].push(i);
+    let letter = sentence[i];
+    if (letter !== " ") {
+      if (results[letter]) {
+        results[letter].push(i);
       } else {
-        results[sentence[i]] = [i];
+        results[letter] = [i];
       }
     }
   }

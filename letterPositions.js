8aebbcc5598c9c5bb991ca39f3a8d1754024a05a
WishNavy <du.vyshu@gmail.com>
2024-01-30 16:26:05 -0800

    created letterPositions func

---
+++ letterPositions.js
@@ -0,0 +1,37 @@
+const assertArraysEqual = function(array1,array2) {
+  if (eqArrays(array1,array2)) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]] = [i];
+      }
+    }
+  }
+  return results;
+};
+
+assertArraysEqual(letterPositions("hello").l, [2,3]);
+console.log(letterPositions("lighthouse in the house"));

2351ba31bf947f100d81aa21043257a224301206
WishNavy <du.vyshu@gmail.com>
2024-01-31 18:25:57 -0800

    made a change

--- eqArrays.js
+++ eqArrays.js
@@ -6,18 +6,18 @@ const assertEqual = function(actual, expected) {
     }
   };
 
-  const eqArrays = function(arr1, arr2){
-    if(arr1.length === arr2.length){
-      for(let i = 0; i < arr1.length; i++){
-        if(arr1[i] !== arr2[i]){
-            return false;
-        }    
+  const eqArrays = function(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+      return false;
+    } else {
+      for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+          return false;
+        }
       }
-      return true;
-    }else{
-        return false;
     }
-  }
+    return true;
+  };
 
 console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
 console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false

8715d3dc9da9f54d98888a255bd121c16ba3da28
WishNavy <du.vyshu@gmail.com>
2024-01-31 18:31:46 -0800

    created assertObjectsEqual func

---
+++ assertObjectsEqual.js
@@ -0,0 +1,57 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const eqObjects = function(object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+  if (keys1.length !== keys2.length) {
+    return false;
+  } else {
+    for (const key of keys1) {
+      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+        if (!eqArrays(object1[key], object2[key])) {
+          return false;
+        }
+      } else {
+        if (object1[key] !== object2[key]) {
+          return false;
+        }
+      }
+    }
+  }
+  return true;
+  
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
+
+const obj1 = {
+  a: [1, 2, 3],
+  b: 4,
+  c: [5, 6]
+};
+const obj2 = {
+  a: [1, 2, 3],
+  b: 4,
+  c: [5, 6]
+};
+assertObjectsEqual(obj1, obj2);
+
+  

09639bef40dfd94539cc4924ea8a211bd9b9f773
WishNavy <du.vyshu@gmail.com>
2024-02-02 16:26:15 -0800

    created takeUntil.js

---
+++ takeUntil.js
@@ -0,0 +1,41 @@
+const eqArrays = function(arr1, arr2) { /* compares 2 arrays */
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const assertArraysEqual = function(array1,array2) {
+  if (eqArrays(array1,array2)) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+const takeUntil = function(array, callback) { /* takes array and callback function as arguments */
+  const result = [];
+  for (const item of array) {
+    if (callback(item)) {
+      return result; /* returns when the callback function is true */
+    } else {
+      result.push(item);
+    }
+  }
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);

413317f350b26d358320bf04ab28083391082eb8
WishNavy <du.vyshu@gmail.com>
2024-02-02 17:13:04 -0800

    created findkey function which takes object and function as arguments

---
+++ findKey.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) { //takes object and function as arguments
+  for (const key in object) {
+    if (callback(object[key])) {
+      return key; //returns if callback function is true
+    }
+  }
+};
+
+
+const result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+assertEqual(result1, "noma");

371b59f8bb5e02e91a86400825c328bb6f30abc5
WishNavy <du.vyshu@gmail.com>
2024-02-14 20:23:37 -0800

    refactored using m & c

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,26 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const words = tail(["Yo Yo", "Lighthouse", "Labs"]);
+describe("#tail", () => {
+    it("returns ['Lighthouse', 'Labs'] for ['yo yo', 'Lighthouse', 'Labs']", () => {
+        assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]),['Lighthouse', 'Labs']);
+    });
+    it("has arraylength of 2 for tail(['yo yo', 'Lighthouse', 'Labs'])", () => {
+        assert.strictEqual(tail(["Yo Yo", "Lighthouse", "Labs"]).length, 2);
+    });
+    it("returns [] for ['Hello']", () => {
+        assert.deepEqual(tail(["Hello"]),[]);
+    });
+    it("has arraylength of 0 for tail(['Hello'])", () => {
+        assert.strictEqual(tail(['Hello']).length, 0);
+    });
+})
+
+
+
+
+
+/*const words = tail(["Yo Yo", "Lighthouse", "Labs"]);
 assertEqual(words.length, 2); // check if array length is 2
 assertEqual(words[0], "Lighthouse"); // ensure first element is "Lighthouse"
 assertEqual(words[1], "Labs");
@@ -13,4 +32,4 @@ assertEqual(tail(words1).length, 0);
 
 const words2 = [];
 tail(words2)
-assertEqual(words2.length, 0);
+assertEqual(words2.length, 0);*/

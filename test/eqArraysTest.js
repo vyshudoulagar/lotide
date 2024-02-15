8260dc52f622b3903c1ef55ddef3d3ba8fedb917
WishNavy <du.vyshu@gmail.com>
2024-02-14 18:58:19 -0800

    refactored

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,11 +1,11 @@
 const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 assertEqual(eqArrays([1, 4, 3], [1, 2, 3]), false);

7f80e6630c74cc39ec172ec044dc18e77747621c
WishNavy <du.vyshu@gmail.com>
2024-02-14 19:49:58 -0800

    added mocha and chai

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -2,4 +2,4 @@ const assertArraysEqual = require('../assertArraysEqual');
 
 assertArraysEqual([1, 2, 3, 4], [1, 2, 3, 4]);
 assertArraysEqual([2, 5, 3, 4], [2, 5, 3, 4]);
-assertArraysEqual([1, 2, 3, 4], [1, 2, 3, 4]);
+assertArraysEqual([1, 2, 3, 4], [1, 2, 3]);
